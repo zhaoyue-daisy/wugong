1fb69914c732e8e2f35e3d74356cd1c0e0ac43cc
赵月 <2991607398@qq.com>
2019-07-05 22:07:56 +0800

    创建

---
+++ src/wugong/dugujiujian.java
@@ -0,0 +1,15 @@
+package wugong;
+
+public class dugujiujian {
+
+	public static void main(String[] args) {
+		System.out.println("111111");
+		//System.out.println("222222");
+		//System.out.println("333333");
+		//System.out.println("444444");
+		//System.out.println("555555");
+		//System.out.println("666666");
+
+	}
+
+}
